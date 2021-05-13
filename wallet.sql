7618c8c7c74f35d138bf6c89a1321e7c013df44d
Zhao KK <kikakkz@hotmail.com>
2021-05-13 17:39:05 +0800

    Add config and wallet sql script

---
+++ wallet.sql
@@ -0,0 +1,48 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : 165
+ Source Server Type    : MySQL
+ Source Server Version : 50732
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : wallet
+
+ Target Server Type    : MySQL
+ Target Server Version : 50732
+ File Encoding         : 65001
+
+ Date: 30/01/2021 11:40:56
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+create database if not exists `wallet`; SET character_set_client = utf8; use wallet;
+-- ----------------------------
+-- Table structure for balance_transfer_request
+-- ----------------------------
+CREATE TABLE if not exists `balance_transfer_request` (
+  `id` varchar(64) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `creator` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '',
+  `reviewer` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '',
+  `from` varchar(512) NULL DEFAULT 0 COMMENT '',
+  `to` varchar(512) NULL DEFAULT 0 COMMENT '',
+  `amount` double(6, 2) NULL DEFAULT 0 COMMENT '',
+  `status` varchar(32) NULL DEFAULT 0 COMMENT '',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for balance_withdraw_request
+-- ----------------------------
+CREATE TABLE if not exists `balance_withdraw_request` (
+  `id` varchar(64) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `creator` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '',
+  `reviewer` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '',
+  `owner` varchar(512) NULL DEFAULT 0 COMMENT '',
+  `miner` varchar(512) NULL DEFAULT 0 COMMENT '',
+  `amount` double(6, 2) NULL DEFAULT 0 COMMENT '',
+  `status` varchar(32) NULL DEFAULT 0 COMMENT '',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
